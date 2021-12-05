03d63f827f56addc4b818161db4cebe3ceec8ff9
NAVACO-CO\m.hekmatizadeh <https://github.com/rasagroups/>
2021-12-05 17:10:32 +0330

    DYNAMIC111

--- app/services/notifications/Notification.php
+++ app/services/notifications/Notification.php
@@ -3,16 +3,15 @@
 
 namespace App\services\notifications;
 
-
-use App\Models\User;
+/*use App\Models\User;
 use App\services\notifications\providers\EmailProvider;
 use App\services\notifications\providers\SmsProvider;
 use Illuminate\Mail\Mailable;
-use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Mail;*/
 
 class Notification
 {
-    public function sendEmail(User $user ,Mailable $mailable)
+ /*   public function sendEmail(User $user ,Mailable $mailable)
     {
             $emailprovider = new EmailProvider() ;
             return $emailprovider->send($user,$mailable);
@@ -22,14 +21,15 @@ class Notification
     {
         $sms = new SmsProvider() ;
         return $sms->send($mobileNumbers,$messages);
-    }
-
+    }*/
+    //الان هر کدوم از providerharo صدا بزنیم به راهتی اجرا میشن و نیاز به تغییر در این کلاس دیگه کلا نیست...
     public function __call($method,$arg)
     {
+        //services class adress for crate obj...
+        //substr($method,4) . 'Provider' => class name
         $providerPath= __NAMESPACE__ . '\providers\\' . substr($method,4) . 'Provider';
-        //dd(...$arg);
+        //create obj for receve to method...
         $providerInstane = new $providerPath;
         $providerInstane->send(...$arg);
-       // echo($providerPath);
     }
 }
